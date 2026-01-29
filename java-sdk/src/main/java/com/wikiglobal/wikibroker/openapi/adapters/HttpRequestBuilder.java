856873d8116724237c9262424f38c9444b00a0ff
auston <auston@wikiglobal.com>
2026-01-29 18:26:33 +0800

    feat: 在java-sdk中添加原生http请求客户端适配模块

---
+++ java-sdk/src/main/java/com/wikiglobal/wikibroker/openapi/adapters/HttpRequestBuilder.java
@@ -0,0 +1,103 @@
+package com.wikiglobal.wikibroker.openapi.adapters;
+
+import com.wikiglobal.wikibroker.openapi.common.types.*;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+public class HttpRequestBuilder implements RequestOperator<HttpRequest>, Wrapper<HttpRequest.Builder> {
+    private final HttpRequest.Builder raw;
+    private final Map<String, String> headers;
+    private String method;
+    private String url;
+    private String body;
+    private final UUID apiKey;
+    private final String apiSecret;
+    private final LoadHeaders<HttpRequest> loadHeaders;
+    private final Sign<HttpRequest> sign;
+    private final Supplier<Instant> timestampGenerator;
+    private final Supplier<UUID> idGenerator;
+
+    public HttpRequestBuilder(
+            HttpRequest.Builder raw,
+            UUID apiKey,
+            String apiSecret,
+            LoadHeaders<HttpRequest> loadHeaders,
+            Sign<HttpRequest> sign,
+            Supplier<Instant> timestampGenerator,
+            Supplier<UUID> idGenerator
+    ) {
+        this.raw = raw;
+        this.headers = new HashMap<>();
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+        this.loadHeaders = loadHeaders;
+        this.sign = sign;
+        this.timestampGenerator = timestampGenerator;
+        this.idGenerator = idGenerator;
+    }
+
+    public HttpRequestBuilder setHeader(String name, String value) {
+        this.headers.put(name, value);
+        this.raw.setHeader(name, this.headers.get(name));
+        return this;
+    }
+
+    public HttpRequestBuilder setMethod(String method) {
+        this.method = method.toUpperCase();
+        this.raw.method(this.method, HttpRequest.BodyPublishers.noBody());
+        return this;
+    }
+
+    public HttpRequestBuilder setUrl(String url) {
+        this.url = url;
+        this.raw.uri(URI.create(this.url));
+        return this;
+    }
+
+    public HttpRequestBuilder setBody(String data) {
+        this.body = data;
+        this.raw.method(this.method, HttpRequest.BodyPublishers.ofString(data));
+        return this;
+    }
+
+    public HttpRequest build() throws MalformedURLException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
+        this.loadHeaders.accept(
+                this,
+                this.apiKey,
+                this.timestampGenerator.get(),
+                this.idGenerator.get()
+        );
+        this.sign.accept(this, this.apiSecret);
+        return this.raw.build();
+    }
+
+    public HttpRequest.Builder raw() {
+        return this.raw;
+    }
+
+    public String getHeader(String name) {
+        return this.headers.get(name);
+    }
+
+    public String getMethod() {
+        return this.method;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+}
--- java-sdk/src/test/java/com/wikiglobal/wikibroker/openapi/OpenApiTest.java
+++ java-sdk/src/test/java/com/wikiglobal/wikibroker/openapi/OpenApiTest.java
@@ -1,16 +1,73 @@
 package com.wikiglobal.wikibroker.openapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.wikiglobal.wikibroker.openapi.adapters.HttpRequestBuilder;
+import com.wikiglobal.wikibroker.openapi.common.enums.CustomHeaders;
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.UUID;
 
-/**
- *
- * @author auston
- */
 public class OpenApiTest {
+    private static final String baseURL = "https://api.example.com";
+    private static final String path = "test?q1=c&q2=b&q1=a";
+
+    private static String url() {
+        return String.format("%s/%s", OpenApiTest.baseURL, OpenApiTest.path);
+    }
+
+    private static final String body = "{\"key\":\"value\"}";
+
+
+    private static final UUID apiKey = UUID.fromString("ef05e5b0-9daf-49e3-a0f4-9a3c13f55c3b");
+    private static final String apiSecret = "4ae4bf20-0afa-4122-ade8-c0beca7bd5e4";
+    private static final UUID nonce = UUID.fromString("4428a206-1afd-4b15-a98d-43e91f49a08d");
+    private static final Instant timestamp = Instant.ofEpochMilli(1798115622000L);
+    private static final String expectedSignature = "1b0c80dbbc30905719559ab5526dfd59bae04d7337c8843efd9e51ff0af6dfb4";
 
     public OpenApiTest() {
     }
 
+    @Test
+    void testNative() {
+        final var raw = HttpRequest.newBuilder();
+        final var builder = new HttpRequestBuilder(
+                raw,
+                apiKey,
+                apiSecret,
+                WikiBrokerOpenApi::addXHeaders,
+                WikiBrokerOpenApi::sign,
+                () -> timestamp,
+                () -> nonce
+        );
+        try {
+            final var req = builder.setUrl(url()).setMethod("POST").setBody(body).build();
+            final var actualSignature = req.headers()
+                                           .firstValue(CustomHeaders.Signature.value())
+                                           .orElse("");
+            assertEquals(expectedSignature, actualSignature);
+        } catch (MalformedURLException |
+                 URISyntaxException |
+                 NoSuchAlgorithmException |
+                 InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testApache() {
+        // TODO
+    }
+
+    @Test
+    void testOkHttp() {
+        // TODO
+    }
 }
