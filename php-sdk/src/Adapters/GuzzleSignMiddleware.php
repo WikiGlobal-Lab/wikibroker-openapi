472c4ebd6334d5af6b9583c0279fe145ed0c7191
auston <auston@wikiglobal.com>
2026-03-04 19:19:47 +0800

    refactor: 优化php-sdk中适配器模块实现

--- php-sdk/src/Adapters/GuzzleSignMiddleware.php
+++ php-sdk/src/Adapters/GuzzleSignMiddleware.php
@@ -5,42 +5,39 @@ declare(strict_types=1);
 namespace WikibrokerOpenapiSdk\Adapters;
 
 use Closure;
-use Psr\Http\Client\ClientInterface;
+use DateTimeInterface;
 use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 
-class HttpClientWithSign implements ClientInterface
-{
+class GuzzleSignMiddleware {
     /**
-     * @param ClientInterface $rawClient
      * @param string $apiKey
      * @param string $apiSecret
      * @param callable(RequestInterface $req, string $apiKey, DateTimeInterface $timestamp, string $nonce):RequestInterface $withHeaders
      * @param callable(RequestInterface $req, string $key):RequestInterface $withSign
      * @param callable():DateTimeInterface $timestampGenerator
      * @param callable():string $idGenerator
-     * @return ClientInterface
+     * @return callable(callable $handler):Closure
      */
     public function __construct(
-        private ClientInterface $rawClient,
         private string $apiKey,
         private string $apiSecret,
         private Closure $withHeaders,
         private Closure $withSign,
         private Closure $timestampGenerator,
         private Closure $idGenerator
-    ) {}
+    ) {
+    }
 
-    public function sendRequest(RequestInterface $request): ResponseInterface
-    {
-
-        $request = ($this->withHeaders)(
-            $request,
-            $this->apiKey,
-            ($this->timestampGenerator)(),
-            ($this->idGenerator)()
-        );
-        $request = ($this->withSign)($request, $this->apiSecret);
-        return $this->rawClient->sendRequest($request);
+    public function __invoke(callable $handler) {
+        return function (RequestInterface $request, array $options) use ($handler) {
+            $request = ($this->withHeaders)(
+                $request,
+                $this->apiKey,
+                ($this->timestampGenerator)(),
+                ($this->idGenerator)()
+            );
+            $request = ($this->withSign)($request, $this->apiSecret);
+            return $handler($request, $options);
+        };
     }
 }
---
+++ php-sdk/src/Adapters/PsrHttpClientWithSign.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WikibrokerOpenapiSdk\Adapters;
+
+use Closure;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class PsrHttpClientWithSign implements ClientInterface {
+    /**
+     * @param ClientInterface $rawClient
+     * @param string $apiKey
+     * @param string $apiSecret
+     * @param callable(RequestInterface $req, string $apiKey, DateTimeInterface $timestamp, string $nonce):RequestInterface $withHeaders
+     * @param callable(RequestInterface $req, string $key):RequestInterface $withSign
+     * @param callable():DateTimeInterface $timestampGenerator
+     * @param callable():string $idGenerator
+     * @return ClientInterface
+     */
+    public function __construct(
+        private ClientInterface $rawClient,
+        private string $apiKey,
+        private string $apiSecret,
+        private Closure $withHeaders,
+        private Closure $withSign,
+        private Closure $timestampGenerator,
+        private Closure $idGenerator
+    ) {
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface {
+
+        $request = ($this->withHeaders)(
+            $request,
+            $this->apiKey,
+            ($this->timestampGenerator)(),
+            ($this->idGenerator)()
+        );
+        $request = ($this->withSign)($request, $this->apiSecret);
+        return $this->rawClient->sendRequest($request);
+    }
+}
