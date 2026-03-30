4f7297c103a7918b1fb06caf54ad525ac1a5c055
auston <auston@wikiglobal.com>
2026-03-30 15:54:29 +0800

    test: 完善优化dotnet-sdk测试

--- dotnet-sdk/tests/WikiBrokerOpenApiTest.cs
+++ dotnet-sdk/tests/WikiBrokerOpenApiTest.cs
@@ -1,5 +1,7 @@
+using System.Net.Http.Json;
 using System.Text.Json;
 using WikiBroker.OpenApi.Sdk;
+using WikiBroker.OpenApi.Sdk.Adapters;
 using WikiBroker.OpenApi.Sdk.Common.Enums;
 
 namespace WikiBroker.OpenApi.Tests
@@ -15,14 +17,9 @@ namespace WikiBroker.OpenApi.Tests
         private const string BaseUrl = "https://api.example.com";
         private const string Path = "test?q1=c&q2=b&q1=a";
 
-        private static string Url()
-        {
-            return $"{BaseUrl}/{Path}";
-        }
+        private static string Url => $"{BaseUrl}/{Path}";
 
-        private record FakeRequestBody(string Key);
-
-        private static readonly FakeRequestBody Body = new("value");
+        private static readonly dynamic Body = new { key = "value" };
         private const string Method = "POST";
         private static readonly Guid ApiKey = Guid.Parse("ef05e5b0-9daf-49e3-a0f4-9a3c13f55c3b");
         private const string ApiSecret = "4ae4bf20-0afa-4122-ade8-c0beca7bd5e4";
@@ -32,16 +29,32 @@ namespace WikiBroker.OpenApi.Tests
         private const string ExpectedSignature = "1b0c80dbbc30905719559ab5526dfd59bae04d7337c8843efd9e51ff0af6dfb4";
 
         [Fact]
-        public async Task TestApi()
+        public void TestApi()
         {
-            var req = new HttpRequestMessage()
+            var handler = new HttpRequestDelegatingHandler(
+                new HttpClientHandler(),
+                ApiKey,
+                ApiSecret,
+                WikiBrokerOpenApi.AddXHeaders,
+                WikiBrokerOpenApi.Sign,
+                () => Timestamp,
+                () => Nonce
+            );
+            var client = new HttpClient(handler);
+            var req = new HttpRequestMessage
             {
-                Content = new StringContent(JsonSerializer.Serialize(Body, Opt)),
+                Content = JsonContent.Create(Body, null, Opt),
                 Method = new HttpMethod(Method),
-                RequestUri = new Uri(Url())
+                RequestUri = new Uri(Url)
             };
-            WikiBrokerOpenApi.AddXHeaders(req, ApiKey, Timestamp, Nonce);
-            await WikiBrokerOpenApi.Sign(req, ApiSecret);
+            try
+            {
+                client.Send(req, new CancellationToken(true));
+            }
+            catch
+            {
+            }
+
             var actualSignature = req.Headers.GetValues(CustomHeaders.Signature.Value).ElementAt(0);
             Assert.Equal(ExpectedSignature, actualSignature);
         }
