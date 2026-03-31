b159612b5b7df231c5d14589ece7c85ef7c6b962
auston <auston@wikiglobal.com>
2026-03-31 15:59:17 +0800

    chore: 搭建dart-sdk项目开发框架

---
+++ dart_sdk/test/wikibroker_openapi_sdk_test.dart
@@ -0,0 +1,16 @@
+import 'package:wikibroker_openapi_sdk/wikibroker_openapi_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
