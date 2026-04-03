faa1919571acc427f8b001f3513ccfb265765fee
auston <auston@wikiglobal.com>
2026-04-03 15:30:26 +0800

    feat: 在dart-sdk中添加高层级API

--- dart-sdk/lib/src/common/utils.dart
+++ dart-sdk/lib/src/common/utils.dart
@@ -1,3 +1,11 @@
+import 'package:uuid/uuid.dart';
+
 bool isRequestUsePostMethod(String method) {
   return method.toUpperCase() == 'POST';
+}
+
+final _uuid = Uuid();
+
+UuidValue newUuid() {
+  return UuidValue.withValidation(_uuid.v4());
 }
--- dart-sdk/lib/src/wikibroker_openapi_sdk_base.dart
+++ dart-sdk/lib/src/wikibroker_openapi_sdk_base.dart
@@ -1,6 +1,10 @@
+import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
+import 'package:wikibroker_openapi_sdk/src/adapters/dio.dart';
+import 'package:wikibroker_openapi_sdk/src/adapters/http.dart';
 import 'package:wikibroker_openapi_sdk/src/common/enums.dart';
 import 'package:wikibroker_openapi_sdk/src/common/types.dart';
+import 'package:wikibroker_openapi_sdk/src/common/utils.dart';
 import 'package:wikibroker_openapi_sdk/src/core.dart';
 
 void addXHeaders(
@@ -19,4 +23,36 @@ void sign(RequestLike req, String key) {
   final canonicalString = generateCanonicalString(req);
   final signature = generateSignature(key, canonicalString);
   req.headers[CustomHeaders.signature.value] = signature;
+}
+
+HttpClient createHttpClient(
+  http.Client raw,
+  UuidValue apiKey,
+  String apiSecret,
+) {
+  return HttpClient(
+    raw,
+    apiKey,
+    apiSecret,
+    addXHeaders,
+    sign,
+    DateTime.now,
+    newUuid,
+  );
+}
+
+DioRequestInterceptor createDioRequestInterceptor(
+  UuidValue apiKey,
+  String apiSecret,
+  String Function(dynamic)? serializer,
+) {
+  return DioRequestInterceptor(
+    serializer ?? (x) => x as String,
+    apiKey,
+    apiSecret,
+    addXHeaders,
+    sign,
+    DateTime.now,
+    newUuid,
+  );
 }
