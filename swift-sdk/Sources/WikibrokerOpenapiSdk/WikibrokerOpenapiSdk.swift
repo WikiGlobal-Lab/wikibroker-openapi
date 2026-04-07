683e5205007474c1cd4d4fb10d4c2bc6efb6632a
biocrosscoder <viscaria_dignics@163.com>
2026-04-07 17:03:03 +0800

    feat: 实现swift-sdk中低层级API

--- swift-sdk/Sources/WikibrokerOpenapiSdk/WikibrokerOpenapiSdk.swift
+++ swift-sdk/Sources/WikibrokerOpenapiSdk/WikibrokerOpenapiSdk.swift
@@ -1,2 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+func addXHeaders(
+    req: inout URLRequest,
+    apiKey: UUID,
+    timestamp: Date,
+    nonce: UUID
+) {
+    req.setValue(
+        apiKey.uuidString,
+        forHTTPHeaderField: CustomHeaders.apiKey.rawValue
+    )
+    req.setValue(
+        String(Int64(timestamp.timeIntervalSince1970 * 1000)),
+        forHTTPHeaderField: CustomHeaders.timestamp.rawValue
+    )
+    req.setValue(
+        nonce.uuidString,
+        forHTTPHeaderField: CustomHeaders.nonce.rawValue
+    )
+}
+
+func sign(req: inout URLRequest, key: String) {
+    let canonicalString = generateCanonicalString(req: req)
+    let signature = generateSignature(key: key, message: canonicalString)
+    req.setValue(
+        signature,
+        forHTTPHeaderField: CustomHeaders.signature.rawValue
+    )
+}
